ea880ea3e03da64e9486b64451920dd9aa99cf38
borisaxel <boris.ebaneth@ossas-it.com>
2025-02-19 03:26:05 +0100

    conf

--- model/config/database.php
+++ model/config/database.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 $dbname = "agencevoyage";
-$dbuser = "exp_user";
-$password = "ossas2023";
+$dbuser = "root";
+$password = "";
 $bdd = new PDO("mysql:host=127.0.0.1;dbname=" . $dbname . ";charset=utf8", "" . $dbuser . "", "" . $password . "");
 if (!$bdd) {
     die("La Connexion a échoué : " . mysqli_connect_error());
